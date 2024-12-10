6508d1628f72461a50902e31f4f324c8c4539214
graptolite <109924401+graptolite@users.noreply.github.com>
2024-12-10 10:17:30 +0000

    Increase consistency with fm3d by splitting sources.in+receivers.in
    - No longer dependent on obsdata.

--- run_fm3d.py
+++ run_fm3d.py
@@ -16,10 +16,6 @@ except:
     cores = 2
 
 fmtomo_bin = "<path to fmtomo bin>"
-try:
-    sources_wd = str(sys.argv[2])
-except:
-    sources_wd = input("Sources working dir must be provided:\n")
 
 def fmtomo(cmd,wd="./"):
     ''' Execute an fmtomo command.
@@ -37,118 +33,78 @@ def fmtomo(cmd,wd="./"):
     os.chdir(owd)
     return
 
-owd = os.getcwd()
-os.chdir(sources_wd)
-fmtomo("obsdata")
-os.chdir(owd)
-shutil.copy2(os.path.join(sources_wd,"sources.in"),"sourcesref.in")
+def modify_receiver_source(receiver_data,new_source):
+    receiver_l = receiver_data.split("\n")
+    receiver_l[2] = "           %u" % int(new_source)
+    return "\n".join(receiver_l)
 
-class InputFile():
-    def __init__(self,path_to_infile):
-        '''
-        path_to_infile | <str> | path to the fmtomo input file.
-        '''
-        self.path = path_to_infile
-        # Load content of the fmtomo input file.
-        with open(path_to_infile) as infile:
-            inputs = infile.read()
-        # Split content into lines.
-        self.lines = inputs.split("\n")
-        return
-    def save(self,path=None):
-        ''' Save the current state of the input lines to disk.
+def load_receiver_dict(receivers_file="receivers.in"):
+    # receivers.in
+    # Skip the first line.
+    # Any line containing a decimal point is the start of a receiver block.
+    with open(receivers_file) as infile:
+        lines = [l for l in infile.read().split("\n")[1:] if l.strip()]
+    receiver_starts = []
+    for i,l in enumerate(lines):
+        # A decimal point in a line means it locates a source.
+        if "." in l:
+            receiver_starts.append(i)
+    receiver_starts.append(len(lines))
+    receivers = ["\n".join(lines[receiver_starts[i]:receiver_starts[i+1]]) for i in range(len(receiver_starts)-1)]
+    receiver_dict = dict()
+    for r in receivers:
+        # The third line will always be the source id.
+        source_id = int(r.split("\n")[2].strip())
+        if source_id in receiver_dict:
+            receiver_dict[source_id].append(r)
+        else:
+            receiver_dict[source_id] = [r]
+    return receiver_dict
 
-        path | <str> | alternative file to save the input lines to (instead of the original file).
+def load_sources_list(sources_file="sources.in"):
+    # sources.in
+    # Skip the first line.
+    # Any line containing a decimal point is the line after the first of a source block.
+    # Any prior line containing no number (i.e. letter phase) shifts the starting line of the block back one.
+    with open(sources_file) as infile:
+        lines = [l for l in infile.read().split("\n")[1:] if l.strip()]
+    source_starts = []
+    for i,l in enumerate(lines):
+        # A decimal point in a line means it locates a source, which is the second line after the start of a source block.
+        if "." in l:
+            if not re.search("[0-9]",lines[i-1]):
+                source_starts.append(i-2)
+            else:
+                source_starts.append(i-1)
+    source_starts.append(len(lines))
+    sources = [(i+1,"\n".join(lines[source_starts[i]:source_starts[i+1]])) for i in range(len(source_starts)-1)]
+    return sources
 
-        Returns: None
-        '''
-        # Check whether a save path has been provided, and if not, use the original filepath.
-        if not path:
-            path = self.path
-        # Save the input lines to the filepath.
-        with open(path,"w") as outfile:
-            outfile.write("\n".join(self.lines))
-        return
-    def update(self,update_dict):
-        ''' Update the value (but keep the comment) for a specified line.
-
-        update_dict | <dict> {<int>:<str>} | dictionary specifying the nature of the updates in the format {line number:"what to update the line with"}.
-
-        Returns: None
-        '''
-        # Iterate through the lines to be updated.
-        for line,repl in update_dict.items():
-            # Replace the value (in front of the comment) on the active line with the new value.
-            self.lines[line-1] = repl + " "*20 + "c:" + self.lines[line-1].split("c:")[-1]
-        return
-    def read(self,line):
-        ''' Read the value from a line.
-
-        line | <int> | line of interest.
-
-        Returns: <str> | value on the line of interest.
-        '''
-        entry = self.lines[line-1].split("c:")[0]
-        return entry
-
-def identify_sources_file(obsdata_path):
-    obsdata_in = InputFile(obsdata_path)
-    sources_file = obsdata_in.read(16).strip()
-    is_local = obsdata_in.read(17).strip() == "0"
-    return sources_file,is_local
-
-def normalize_obsdata(obsdata_path):
-    obsdata_in = InputFile(obsdata_path)
-    for l in [6,7,8,12]:
-        path = obsdata_in.read(l).strip()
-        obsdata_in.update({l:os.path.split(path)[-1]})
-        obsdata_in.save()
-    return
-
-def split_sources(sources_wd,cores):
-    obsdata_sources_input,is_local = identify_sources_file(os.path.join(sources_wd,"obsdata.in"))
-    normalize_obsdata(os.path.join(sources_wd,"obsdata.in"))
-    print(obsdata_sources_input,is_local)
-    original_owd = os.getcwd()
-    with open(os.path.join(sources_wd,obsdata_sources_input)) as infile:
-        source_lines = [l for l in infile.read().split("\n")[1:] if l.strip()]
-    source_data = []
-    # Determine number of lines per source based on whether it's local or not.
-    lines_per_source = 3 if is_local else 1
-    for i in range(int(len(source_lines)/lines_per_source)):
-        source_data.append("\n".join(source_lines[i*lines_per_source:(i+1)*lines_per_source]))
-
-    swap_wd = os.path.join(sources_wd,".tmp")
-    if not os.path.exists(swap_wd):
-        os.mkdir(swap_wd)
-    else:
-        for f in os.listdir(swap_wd):
-            shutil.rmtree(os.path.join(swap_wd,f))
-
-    owd = sources_wd
-    source_sublists = np.array_split(source_data,cores)
+def split_sources(cores):
+    tmp = ".tmp"
+    if not os.path.exists(tmp):
+        os.mkdir(tmp)
+    receiver_dict = load_receiver_dict()
+    sources = load_sources_list()
+    distrib_s = np.array_split(sources,cores)
     wds = []
-    picks_parent = os.path.join(sources_wd,"picks")
-    for i,sl in enumerate(source_sublists):
-        tmp_wd = os.path.join(swap_wd,str(i))
-        if not os.path.exists(tmp_wd):
-            os.mkdir(tmp_wd)
-        for f in ["obsdata.in"]:
-            if not os.path.isdir(f):
-                shutil.copy2(os.path.join(sources_wd,f),os.path.join(tmp_wd,f))
-        sl = list(sl)
-        sl = [str(len(sl))] + sl
-        with open(os.path.join(tmp_wd,obsdata_sources_input),"w") as outfile:
-            outfile.write("\n".join(sl))
-        picks_tmp = os.path.join(tmp_wd,"picks")
-        os.mkdir(picks_tmp)
-        for f in os.listdir(picks_parent):
-            shutil.copy2(os.path.join(picks_parent,f),os.path.join(picks_tmp,f))
-        os.chdir(tmp_wd)
-        fmtomo("obsdata")
-        os.chdir(owd)
-        wds.append(tmp_wd)
-    os.chdir(original_owd)
+    for i,sub_s in enumerate(distrib_s):
+        t_wd = os.path.join(tmp,str(i))
+        if not os.path.exists(t_wd):
+            os.mkdir(t_wd)
+        with open(os.path.join(t_wd,"sources.in"),"w") as outfile:
+            outfile.write("\n".join([str(len(sub_s))] + [x[1] for x in sub_s]))
+        source_ids = [int(x[0]) for x in sub_s]
+        norm_source_ids = np.array(source_ids) - source_ids[0] + 1
+        active_receivers = []
+        for s_id,n_s_id in zip(source_ids,norm_source_ids):
+            s_receivers = receiver_dict[s_id]
+            if s_id != n_s_id:
+                s_receivers = [modify_receiver_source(r,n_s_id) for r in s_receivers]
+            active_receivers.extend(s_receivers)
+        with open(os.path.join(t_wd,"receivers.in"),"w") as outfile:
+            outfile.write("\n".join([str(len(active_receivers))] + active_receivers))
+        wds.append(t_wd)
     return wds
 
 def combine_arrivals(out,fs):
@@ -200,17 +156,18 @@ def combine_frechet(out,frechs):
 
 print("running on",cores,"cores")
 
-pick_wds = split_sources(sources_wd,cores)
-
 active_dir = ".tmp"
 if not os.path.exists(active_dir):
     os.mkdir(active_dir)
 for f in os.listdir(active_dir):
     shutil.rmtree(os.path.join(active_dir,f))
 
+pick_wds = split_sources(cores)
+
 def execute(i):
     working_dir = os.path.join(active_dir,str(i))
-    os.mkdir(working_dir)
+    if not os.path.exists(working_dir):
+        os.mkdir(working_dir)
     # Copy grids.
     files = ["frechgen.in","frechet.in","interfaces.in","propgrid.in","vgrids.in","mode_set.in","ak135.hed","ak135.tbl"]
     for f in files:
@@ -220,9 +177,6 @@ def execute(i):
             print("Failed to find file",f)
     shutil.copy2("interfaces.in",os.path.join(working_dir,"interfacesref.in"))
     shutil.copy2("vgrids.in",os.path.join(working_dir,"vgridsref.in"))
-    files = ["sources.in","receivers.in","otimes.dat"]
-    for f in files:
-        shutil.copy2(os.path.join(pick_wds[i],f),working_dir)
     owd = os.getcwd()
     os.chdir(working_dir)
     fmtomo("fm3d")
